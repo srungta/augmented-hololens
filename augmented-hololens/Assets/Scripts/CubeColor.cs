5f24840b291265046c78db5db5e62eed3fbd9c6d
Shaswat Rungta <shaswatrungta@hotmail.com>
2018-03-07 21:15:50 +0530

    Added Random color generator

---
+++ augmented-hololens/Assets/Scripts/CubeColor.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class CubeColor : MonoBehaviour
+{
+
+    private Color GetRandomColor()
+    {
+        return new Color(GetRandomFloatValue(), GetRandomFloatValue(), GetRandomFloatValue());
+    }
+
+    private float GetRandomFloatValue(float min = 0.0f, float max = 1.0f)
+    {
+        return Random.Range(min, max);
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        var newColor = GetRandomColor();
+        this.GetComponent<Renderer>().material.color = newColor;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
